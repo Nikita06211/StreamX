ab890b2f21475b4b873d674ef636dc82354bb5fb
Nikita <bansalnikita06@gmail.com>
2025-04-02 16:11:05 +0530

    added register controller and bug fixes

--- src/index.js
+++ src/index.js
@@ -1,44 +1,20 @@
-// require('dotenv').config({path:'/env'});
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
+    path: "./.env", 
+});
 
 connectDB()
-.then(process.env.PORT || 8000, ()=>{
-    console.log(`Server is running at PORT ${process.env.PORT}`);
-})
-.catch((err)=>{
-    console.log("MongoDB connection fail",err);
-})
-
-
-
-
-
-/*
-import express from "express";
-const app = express()
-
-(async()=>{
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error",()=>{
-            console.log("err: ",error)
-            throw error
-        })
-
-        app.listen(process.env.PORT, ()=>{
-            console.log(`App is listening on port ${process.env.PORT}`);
-        })
-
-    } catch (error) {
-        console.error(error);
-    }
-})()
-
-*/
+  .then(() => {
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => {
+      console.log(`✅ Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed", err);
+  });
